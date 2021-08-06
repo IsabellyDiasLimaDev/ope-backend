56cf52a1d99a9a5a8463e96c4cf08d2512d7e4d8
Isabelly Dias <isabellydias29@gmail.com>
2021-08-06 19:01:35 -0300

    projeto inicial spring

---
+++ gerenciador-orcamento/src/main/java/br/com/giorni/gerenciadororcamento/GerenciadorOrcamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.giorni.gerenciadororcamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorOrcamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorOrcamentoApplication.class, args);
+	}
+
+}
---
+++ gerenciador-orcamento/src/test/java/br/com/giorni/gerenciadororcamento/GerenciadorOrcamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giorni.gerenciadororcamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorOrcamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
